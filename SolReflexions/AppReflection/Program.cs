f298ff882c43a1ca4e857ecb8e9e0a01753809f3
rcsoliz <robertoucbnh@gmail.com>
2021-04-24 21:46:55 -0400

    Segunda version refactizando code

--- SolReflexions/AppReflection/Program.cs
+++ SolReflexions/AppReflection/Program.cs
@@ -20,6 +20,15 @@ namespace AppReflection
                 //cargamos el asembly
                 asmInitial = Assembly.Load("Business");
                 asmBus = Assembly.Load("Initial");
+
+                getAssembly(asmInitial, "Business.Employees", "getInformation", "result", "Marcela", "Antelo", "895612", "Santa Cruz", "Bolivia");
+               
+                Console.WriteLine("");
+                Console.WriteLine("-------------------------------------------------");
+
+                getAssembly(asmBus, "Initial.Customer", "getInformation", "result", "Juan", "Perez", "689524", "Cancun", "Mexico");
+                Console.Read();
+
             }
             catch (Exception ex)
             {
@@ -27,80 +36,43 @@ namespace AppReflection
                 return;
             }
 
-            if (asmInitial != null)
+        }
+
+        public static void getAssembly(Assembly asm, string nameClass, string nameMethod, string propResult, string name, string surName, string phone, string city, string country)
+        {
+            if (asm != null)
             {
                 // obtenemos el typo que no es mas que la clase con la que desemos trabajar
-                Type employess = asmInitial.GetType("Business.Employees");
-
+                Type objClass = asm.GetType(nameClass);
                 try
                 {
                     //creamos la instancia
-                    object obj = Activator.CreateInstance(employess, new object[] { });
+                    object obj = Activator.CreateInstance(objClass, new object[] { });
                     Console.WriteLine("Tenemos instancia de {0}", obj);
 
                     //obtenemos el metodo
-                    MethodInfo getInformation = employess.GetMethod("getInformation");
+                    MethodInfo getInformation = objClass.GetMethod(nameMethod);
 
                     //obtenemos las propiedades
-                    PropertyInfo resultado = employess.GetProperty("result");
+                    PropertyInfo resultado = objClass.GetProperty(propResult);
 
                     string respuesta = string.Empty;
 
                     //invocamos al metodo 
-                    getInformation.Invoke(obj, new object[] { "Marcela", "Antelo", "895612", "Santa Cruz", "Bolivia" });
+                    getInformation.Invoke(obj, new object[] { name, surName, phone, city, country });
 
                     respuesta = (string)resultado.GetValue(obj);
 
                     //Imprimimos la respuesta
                     Console.WriteLine("El resultado es: {0} ", respuesta);
-
-
                 }
                 catch (Exception ex)
                 {
                     Console.WriteLine(ex.Message);
                 }
             }
-            Console.WriteLine("");
-            Console.WriteLine("-------------------------------------------------");
-            
-            if(asmBus != null)
-            {
-                // obtenemos el typo que no es mas que la clase con la que desemos trabajar
-                Type customer = asmBus.GetType("Initial.Customer");
-                try
-                {
-                    //creamos la instancia
-                    object obj = Activator.CreateInstance(customer, new object[] { });
-                    Console.WriteLine("Tenemos instancia de {0}", obj);
-
-                    //obtenemos el metodo
-                    MethodInfo getInformation = customer.GetMethod("getInformation");
-
-                    //obtenemos las propiedades
-                    PropertyInfo resultado = customer.GetProperty("result");
-
-                    string respuesta = string.Empty;
-
-                    //invocamos al metodo 
-                    getInformation.Invoke(obj, new object[] { "Juan", "Perez", "689524", "Cancun", "Mexico" });
-
-                    respuesta = (string)resultado.GetValue(obj);
-
-                    //Imprimimos la respuesta
-                    Console.WriteLine("El resultado es: {0} ", respuesta);
-
-                }
-                catch(Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-            }
-
-
-            Console.Read();
-
-
         }
+
+
     }
 }
